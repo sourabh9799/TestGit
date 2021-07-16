d0552ad5b12ec937298fe910070aadc87497ec82
sourabh9799 <sourabh.patil9028@gmail.com>
2021-07-16 18:59:09 +0530

    First Commit

---
+++ GitTest/src/com/neosoft/gitTest/GitTest.java
@@ -0,0 +1,11 @@
+package com.neosoft.gitTest;
+
+public class GitTest {
+	String code="Se";
+	if(code=="Se") {
+		System.out.println("Software Engineer");
+	}
+	else {
+		System.out.println("Not a Software Engineer");
+	}
+}
