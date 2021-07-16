9ebb4ab7fd2ca8b2ca758c5b92b3c76505deaf32
sourabh9799 <sourabh.patil9028@gmail.com>
2021-07-16 19:08:34 +0530

    new changes

--- GitTest/src/com/neosoft/gitTest/GitTest.java
+++ GitTest/src/com/neosoft/gitTest/GitTest.java
@@ -1,11 +1,14 @@
 package com.neosoft.gitTest;
 
 public class GitTest {
-	String code="Se";
-	if(code=="Se") {
-		System.out.println("Software Engineer");
-	}
-	else {
-		System.out.println("Not a Software Engineer");
+	public static void main(String args[])
+	{
+		String code="Se";
+		if(code=="Se") {
+			System.out.println("Software Engineer");
+		}
+		else {
+			System.out.println("Not a Software Engineer");
+		}
 	}
 }
